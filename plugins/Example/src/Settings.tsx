57e993a2d9351047f021923eb32ae8c6fa8c44a3
Natalie B <22222885+espeon@users.noreply.github.com>
2025-05-26 20:50:47 -0500

    Implement native API server for example plugin
    Add an HTTP server within the example plugin to serve current
    playback information and frontend assets for an external viewer.
    
    This includes:
    - New API implementation in `serveApi.native.ts`.
    - Functions to get playback time and update server state.
    - Listeners for media transitions and play state changes.
    - Polling to update playback position.
    - Updates to `package.json` and dependencies.

--- plugins/Example/src/Settings.tsx
+++ plugins/Example/src/Settings.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-import { InfoMessage } from "@luna/lib";
+import {} from "@luna/lib";
 import { LunaSwitchSetting } from "@luna/ui";
 
 export const Settings = () => {
-	const onChange = React.useCallback((_: React.ChangeEvent<HTMLInputElement>, checked?: boolean) => {
-		InfoMessage(`Example switch is now ${checked ? "on" : "off"}`);
-	}, []);
-	return <LunaSwitchSetting title="Example Switch" desc="This is an example switch" onChange={onChange} />;
+  return (
+    <LunaSwitchSetting
+      title="Example Switch"
+      desc="This is an example switch"
+    />
+  );
 };
--- plugins/Example/src/index.ts
+++ plugins/Example/src/index.ts
@@ -1,19 +1,152 @@
-import { InfoMessage, redux, Signal, type LunaUnload } from "@luna/lib";
+import type { LunaUnload } from "@luna/core";
+import { redux, MediaItem, PlayState, ipcRenderer } from "@luna/lib";
+
 import { intercept } from "plugins/lib/src/redux";
+import { getCurrentReduxTime } from "./time";
+import { startServer, stopServer, updateMediaInfo } from "./serveApi.native";
 
-InfoMessage(`Hello ${redux.store.getState().user.meta.profileName} from the Example plugin!`);
-
-// Example plugin settings
-export { Settings } from "./Settings";
-
-// Functions in unloads are called when plugin is unloaded.
-// Used to clean up resources, even listener dispose etc should be added here
 export const unloads = new Set<LunaUnload>();
 
-// Log to console whenever changing page
-intercept("page/SET_PAGE_ID", unloads, console.log);
+interface MediaInfo {
+  /// the current track
+  item: any | null;
+  /// the playhead position in seconds
+  position: number | null;
+  /// the duration of the current track in seconds
+  duration: number | null;
+  /// an art URL for the current track's album
+  albumArt: string | null;
+  /// an art URL for the current track's artist
+  artistArt: string | null;
+  /// is the player paused
+  paused: boolean;
+  lastUpdate: number;
+}
 
-// Error signal allowing us to show error messages in the UI
-export const errSignal = new Signal<string | undefined>(undefined);
+interface UpdateInfo {
+  track?: MediaItem;
+  time?: number;
+  paused?: boolean;
+}
 
-errSignal._ = "Example plugin error signal";
+let currentInfo: MediaInfo | null = null;
+
+const getMediaURL = (id?: string, path = "/1280x1280.jpg") =>
+  id
+    ? "https://resources.tidal.com/images/" + id.split("-").join("/") + path
+    : null;
+
+export const update = async (info?: UpdateInfo) => {
+  if (!info) {
+    return;
+  }
+
+  if (!currentInfo) {
+    currentInfo = {
+      item: null,
+      position: 0,
+      duration: null,
+      albumArt: null,
+      artistArt: null,
+      paused: false,
+      lastUpdate: Date.now(),
+    };
+  }
+
+  if (info.track) {
+    currentInfo.item = info.track.tidalItem;
+    console.log("Track changed:", currentInfo.item);
+    const track = currentInfo.item;
+    currentInfo.albumArt = await info.track.coverUrl();
+    currentInfo.artistArt =
+      track.artist && track.artist.picture
+        ? getMediaURL(track.artist.picture, "/320x320.jpg")
+        : null;
+
+    currentInfo.duration = track.duration;
+  }
+
+  // Handle time and paused state updates (from polling)
+  if (info.time !== undefined) {
+    currentInfo.position = info.time;
+    currentInfo.lastUpdate = Date.now();
+  }
+
+  if (info.paused !== undefined) {
+    currentInfo.paused = info.paused;
+  }
+
+  console.log("Current playback state:", currentInfo);
+
+  // Ensure we have some minimal info before sending
+  if (currentInfo.item || currentInfo.position !== null) {
+    console.log("Sending update");
+    updateMediaInfo(currentInfo);
+  } else {
+    console.log("Not enough info to send update.");
+  }
+};
+
+// forever
+(() => {
+  console.log(redux);
+  const printPlayTime = async () => {
+    let info = {
+      time: PlayState.paused
+        ? redux.store.getState().playbackControls.latestCurrentTime
+        : getCurrentReduxTime(),
+      paused: PlayState.paused,
+    };
+    update(info);
+    // wait for 1 second
+    let t = setTimeout(printPlayTime, 1000);
+    unloads.add(() => clearTimeout(t));
+  };
+  printPlayTime();
+})();
+
+MediaItem.onMediaTransition(unloads, async (mediaItem) => {
+  console.log("Media transitioned to " + (await mediaItem.title()));
+  update({
+    track: mediaItem,
+  });
+});
+
+PlayState.onState(unloads, (state) => {
+  console.log("Play state changed to " + state);
+  update({
+    paused: state !== "PLAYING",
+  });
+});
+console.log(MediaItem);
+
+export const onLoad = () => {
+  console.log("Loading EddyAPI on port " + 3665);
+  console.log("Secure mode " + "disabled.");
+  try {
+    server = startServer({
+      port: 3665,
+      secure: false,
+      apiKey: undefined,
+    });
+    console.log("EddyAPI started");
+    unloads.add(() => stopServer());
+    unloads.add(() => {
+      stopServer();
+    });
+
+    // finally, init the media info store
+    const initStore = async () => {
+      const item = await MediaItem.fromPlaybackContext();
+      update({
+        track: item,
+      });
+      console.log("Initial media info:", item);
+    };
+    initStore();
+  } catch (error) {
+    console.error("Failed to start EddyAPI:", error);
+  }
+};
+
+onLoad();
